fd2b30546b9e2d8a74c8a353cb07adfe5c6b2c31
Will Thompson <will.k.t@gmail.com>
2020-02-21 08:32:37 -0600

    update to python template project

---
+++ 7-bulletproof/python-project-template/kelloggrs/about.py
@@ -0,0 +1,5 @@
+# fmt: off
+__title__ = "kelloggrw"
+__version__ = "0.1.1"
+__release__ = False
+__github_url__ = "https://github.com/rs-kellogg/empirical-workshop-2020/tree/master/7-bulletproof/python-project-template"
